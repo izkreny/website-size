bcae760eb310e7f753fe31d1ac09ea65be86de3e
Mario Marić <git@mariomaric.net>
2013-10-07 21:04:40 +0200

    Ready to r'n'r! :)

--- website-size.sh
+++ website-size.sh
@@ -2,12 +2,25 @@
 # Requirements: wget, grep, sed, awk
 # Usage: website-size URL
 
-# One-lineristic wget, grep, sed & awk magic
-echo "Estimated size of the $1 website: $(\
-  wget -r --spider -S -O - "$1" | \
-  grep -e "Content-Length" | \
+# wget log file
+log=/tmp/wget-website-size-log.txt
+
+# Do the spider work
+echo "### Checking size of $1 website... ###"
+sleep 2s
+echo "### This will take some time to finish, please wait. ###"
+wget --recursive --spider --server-response --output-file="$log" "$1"
+echo "Finished!"
+sleep 1s
+
+# Calculate and print website size
+echo "Estimated size of the website: $(\
+  grep -e "Content-Length" "$log" | \
   sed -e 's/  Content-Length: //g' | \
   awk '{sum+=$1} END {printf("%.0f", sum / 1024 / 1024)}'\
 ) Mb"
+
+# Delete wget log file
+rm "$log"
 
 exit

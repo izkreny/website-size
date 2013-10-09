8201ffae61f31994b51198f4a6dd608ea5767820
Mario Marić <git@mariomaric.net>
2013-10-09 20:11:34 +0200

    Added script to estimate size on saved wget-log.

---
+++ analyze-wget-log.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Info: http://mariomaric.net/website-size/
+
+# Check if second argument is provided
+if [ -z "$2" ]
+then
+    # Calculate and print estimated website size
+    echo "Estimated size: $(\
+        grep -e "Content-Length" "$1" | \
+        awk '{sum+=$2} END {printf("%.2f", sum / 1024 / 1024)}'\
+    ) Mb"
+else
+    # Calculate and print estimated size of files
+    echo "Estimated size of $2 files: $(\
+        grep -e "Length" "$1" | grep -e "$2" | grep -v "unspecified" | \
+        awk '{sum+=$2} END {printf("%.2f", sum / 1024 / 1024)}'\
+    ) Mb"
+fi
+
+exit
